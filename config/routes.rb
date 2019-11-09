9ecb41fc94835aa333e2c1cbfe903a3381445fc8
Joe <josephfmck@gmail.com>
2019-11-09 15:41:03 -0600

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  #edit routes.rb to set our root route. Do this to see app without appending /cars at the end of the URL.
+  #added root 'cars#index'    
+  root 'cars#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
